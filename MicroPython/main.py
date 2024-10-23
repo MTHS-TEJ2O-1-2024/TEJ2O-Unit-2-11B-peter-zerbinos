bde04293cdde8911915f1866a4fa861105678cbe
Peter Zerbinos <peter.zerbinos@mths.ca>
2024-10-23 06:25:23 +0000

    micropython added (not formated correctly)

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,47 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+/* Copyright (c) 2024 MTHS All rights reserved
+Created by: Peter Zerbinos
+Created on: Sep 2024
+This program displays the highest number out of two randomly selected numbers.
 """
 
 from microbit import *
+import random
 
+# setup
+display.show(Image.HAPPY)
 
-display.scroll("Hello, World!")
+# variables
+random_number1 = random.randint(0, 99)
+random_number2 = random.randint(0, 99)
+
+# display 1st number
+def display_number1():
+    display.clear()
+    display.scroll("#1:")
+    display.show(random_number1)
+    display.show(Image.HAPPY)
+
+# display 2nd number
+def display_number2():
+    display.clear()
+    display.scroll("#2:")
+    display.show(random_number2)
+    display.show(Image.HAPPY)
+
+# check for button a press
+while True:
+    if button_a.is_pressed():
+        display_number1()
+    
+    if button_b.is_pressed():
+        display_number2()
+
+# shake for highest number
+
+if accelerometer.was_gesture("shake"):
+    display.clear()
+    if random_number1 > random_number2:
+        display.show(random_number1)
+    else:
+        display.show(random_number2)
